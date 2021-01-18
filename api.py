249a0c01e9ba885dff46e240d24d27687e0fab1e
Ilias <ilias.alaoui@student.ecp.fr>
2021-01-18 17:28:48 +0100

    Platform object done, next: tag object

--- api.py
+++ api.py
@@ -1,1 +1,6 @@
-from app import app
+from app import app, db
+from app.models import Platform, Game
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'Platform': Platform, 'Game': Game}
--- app/models.py
+++ app/models.py
@@ -1,10 +1,26 @@
 from app import db
 
 
+class Platform(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True, unique=True)
+    games = db.relationship("Game", backref="platform", lazy='dynamic')
+    logo_url = db.Column(db.String(200))
+
+    def __repr__(self):
+        return f"<Platfrom {self.name}"
+
+
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), index=True, unique=True)
+    platform_id = db.Column(db.Integer, db.ForeignKey("platform.id"))
     poster_url = db.Column(db.String(200))
 
     def __repr__(self):
-        return f"<User {self.name}>"
+        return f"<Game {self.name}>"
+
+
+class Tag(db.Models):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), index=True, unique=True) 
