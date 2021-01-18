75cce60c6fd814e128654b42889bf1c6eaf1b945
Ilias <ilias.alaoui@student.ecp.fr>
2021-01-18 19:30:12 +0100

    tag object done (needs testing), next: start working on routes

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from app import app, db
-from app.models import Platform, Game
+from app.models import Platform, Game, Tag
+
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'Platform': Platform, 'Game': Game}
+    return {'db': db, 'Platform': Platform, 'Game': Game, 'Tag': Tag}
--- app/models.py
+++ app/models.py
@@ -1,26 +1,38 @@
 from app import db
 
+tags = db.Table('tags',
+                db.Column('gmae_id', db.Integer, db.ForeignKey(
+                    'game.id'), primary_key=True),
+                db.Column('tag_id', db.Integer, db.ForeignKey(
+                    'tag.id'), primary_key=True)
+                )
+
 
 class Platform(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), index=True, unique=True)
-    games = db.relationship("Game", backref="platform", lazy='dynamic')
+    name = db.Column(db.String(50), index=True)
+    games = db.relationship("Game", lazy="dynamic")
     logo_url = db.Column(db.String(200))
 
     def __repr__(self):
-        return f"<Platfrom {self.name}"
+        return f"<Platform: {self.name}"
 
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), index=True, unique=True)
-    platform_id = db.Column(db.Integer, db.ForeignKey("platform.id"))
     poster_url = db.Column(db.String(200))
+    platform_id = db.Column(db.Integer, db.ForeignKey("platform.id"))
+    tags = db.relationship(
+        'Tag', secondary=tags, lazy='subquery', backref=db.backref('games', lazy=True))
 
     def __repr__(self):
-        return f"<Game {self.name}>"
+        return f"<Game: {self.name}>"
 
 
-class Tag(db.Models):
+class Tag(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(30), index=True, unique=True) 
+    name = db.Column(db.String(30), index=True, unique=True)
+
+    def __repr__(self):
+        return f"Tag: {self.name}"
